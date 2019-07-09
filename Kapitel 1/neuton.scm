1dfff67f34d48a481c8540150e13ba53b7ce12f0
KONAKONA666 <aybek9221@gmail.com>
2019-07-09 22:47:10 +0200

    fixed Newton

--- Kapitel 1/neuton.scm
+++ Kapitel 1/neuton.scm
@@ -7,7 +7,7 @@
 (define (fixpoint f approx)
   (define tolerance 0.00001)
   (define (close-enough? x y)
-    (> (abs (- x y)) tolerance))
+    (< (abs (- x y)) tolerance))
   (define (calc curr)
     (let ((new_curr (f curr)))
       (if (close-enough? new_curr curr)
