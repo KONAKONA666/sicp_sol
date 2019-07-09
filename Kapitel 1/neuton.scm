d299f8ad4ad1045e5935078af5d8588549e3fbf6
KONAKONA666 <aybek9221@gmail.com>
2019-07-09 22:32:31 +0200

    added Newton Methode

---
+++ Kapitel 1/neuton.scm
@@ -0,0 +1,28 @@
+#lang sicp
+
+
+(define dx 0.0001)
+
+
+(define (fixpoint f approx)
+  (define tolerance 0.00001)
+  (define (close-enough? x y)
+    (> (abs (- x y)) tolerance))
+  (define (calc curr)
+    (let ((new_curr (f curr)))
+      (if (close-enough? new_curr curr)
+          curr
+          (calc new_curr))))
+  (calc approx))
+  
+(define (derivative g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) ((derivative g) x)))))
+
+(define (newton g approx)
+  (fixpoint (newton-transform g) approx))
+
+
